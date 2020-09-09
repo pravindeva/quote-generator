a89a591c90a54ac67f0ec846529b0328749584d7
pravindeva <pravindeva2580@gmail.com>
2020-09-09 22:50:59 +0530

    Updates

---
+++ precache-manifest.5bd48f92daa83eb2829fa81b777e404d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "465fb27731e15b4431e8245a613ebc9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2da15404577f09f60d73",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1f3feb0f36c995e1c717",
+    "url": "/static/css/main.bc8857d0.chunk.css"
+  },
+  {
+    "revision": "2da15404577f09f60d73",
+    "url": "/static/js/2.6bc5da63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6bc5da63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f3feb0f36c995e1c717",
+    "url": "/static/js/main.e49bc718.chunk.js"
+  },
+  {
+    "revision": "363947c9e88dc625d12c",
+    "url": "/static/js/runtime-main.8716e6b8.js"
+  },
+  {
+    "revision": "01a027f7ca665dfc1072eacfa75b2f18",
+    "url": "/static/media/cool-background.01a027f7.png"
+  }
+]);
