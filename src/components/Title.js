f643cce535a69daebae5c24e562641a81ff966f9
pravindeva <30575339+pravindeva@users.noreply.github.com>
2020-09-10 08:46:11 +0530

    Update Title.js
    Margin top

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,8 +4,8 @@ import "./Title.css";
 export default class Title extends Component {
   render() {
     return (
-      <div className="tc dib f3 b header animate__animated animate__jello  animate__infinite mt1 ">
-        <p className="mb1 first">
+      <div className="tc dib f3 b header animate__animated animate__jello  animate__infinite ">
+        <p className="mt2 first">
           <span>P</span>
           <span>O</span>
           <span>S</span>
@@ -15,7 +15,7 @@ export default class Title extends Component {
           <span>V</span>
           <span>E</span>
         </p>
-        <p className="second">
+        <p className=" mt1 second">
           <span>Q</span>
           <span>U</span>
           <span>O</span>
