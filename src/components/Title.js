8679118ba44d1baaa2729fd35af5cf63dd0258a8
pravindeva <30575339+pravindeva@users.noreply.github.com>
2020-09-10 08:31:24 +0530

    Update Title.js
    Removing margin all in positive

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,7 +5,7 @@ export default class Title extends Component {
   render() {
     return (
       <div className="tc dib f3 b header animate__animated animate__jello  animate__infinite mt3 ">
-        <p className="pb1 ma0 first">
+        <p className="pb1 first">
           <span>P</span>
           <span>O</span>
           <span>S</span>
@@ -15,7 +15,7 @@ export default class Title extends Component {
           <span>V</span>
           <span>E</span>
         </p>
-        <p className="pa0 ma0 mb3 second">
+        <p className="pa0 mb3 second">
           <span>Q</span>
           <span>U</span>
           <span>O</span>
