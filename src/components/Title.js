35038d52a306f37efeb5940e297978f0222c5f64
pravindeva <30575339+pravindeva@users.noreply.github.com>
2021-01-05 17:25:36 +0530

    Update Title.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,8 +4,8 @@ import "./Title.css";
 export default class Title extends Component {
   render() {
     return (
-      <div className= "f3 tc b header">
-        <p className="first mb0">
+      <div className= "f4 tc b">
+        <p className="first">
           <span>P</span>
           <span>O</span>
           <span>S</span>
@@ -15,7 +15,7 @@ export default class Title extends Component {
           <span>V</span>
           <span>E</span>
         </p>
-        <p className="mt1 second">
+        <p className="second">
           <span>Q</span>
           <span>U</span>
           <span>O</span>
